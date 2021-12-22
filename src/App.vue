716a61e580bcc57daebe3eb523074cdcd5e472a2
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-12-22 12:38:03 +0000

    src/components/BaseInput.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseInput label="Username" type="text" placeholder="Username" v-model="username"/>
+    <BaseInput label="Password" type="password" placeholder="********" v-model="password"/>
+
+    <hr>
+    {{ username }} - {{ password }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseInput from "./components/BaseInput";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    BaseInput
+  },
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <label>
+      {{ label }}
+      <input
+        :type="type"
+        :placeholder="placeholder"
+        @input="$emit('input', $event.target.value)"
+      >
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    label: String,
+    type: String,
+    placeholder: String
+  }
+};
+</script>
+
